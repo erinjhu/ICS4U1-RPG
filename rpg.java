cf8a54c91d6be0441d5ce927cae31f973ff71342
erin-hu24 <erinjhu@gmail.com>
2023-09-24 18:30:47 -0400

    CSV array and screen display
    Changed method to split values between commas. Added method to display images to screen depending on the value in the map array.

--- rpg.java
+++ rpg.java
@@ -3,11 +3,82 @@
 // Version Number: 1
 
 import arc.*;
+import java.awt.image.BufferedImage;
+
 
 public class rpg{
 	
+	// Main method
+	
 	public static void main(String[] args){
+		// Variables
 		Console con = new Console();
+		String[][] strMap = new String[20][20];
+		// Load map into array
+		strMap = loadMap("map.csv");
+		// Display map on screen
+		displayMap(con, strMap);
+	}
+	
+	// Method to load CSV into array
+	
+	public static String[][] loadMap(String strFileName){
+		// Variables
+		String strMap[][] = new String[20][20];
+		TextInputFile mapFile = new TextInputFile(strFileName);
+		String strLine;
+		String[] strSplit = new String[20];
+		int intRow;
+		int intColumn;
+		// Read file
+		for(intRow = 0; intRow < 20; intRow++){
+			// Read entire line
+			strLine = mapFile.readLine();
+			//System.out.println(strLine);
+			// Split to put values not including commas into array
+			strSplit = strLine.split(",");
+			for(intColumn = 0; intColumn < 20; intColumn++){
+				strMap[intRow][intColumn] = strSplit[intColumn];
+				//System.out.print(strMap[intRow][intColumn]);
+			}
+			//System.out.println();
+		}
+		// Close file
+		mapFile.close();
+		// Return
+		return strMap;
+	}
+	
+	// Method to put map to screen
+	
+	public static void displayMap(Console con, String[][] strMap){
+		// Variables
+		int intRow;
+		int intColumn;
+		// Image variables
+		BufferedImage imgGrass = con.loadImage("grass.png");
+		BufferedImage imgTree = con.loadImage("tree.png");
+		BufferedImage imgWater = con.loadImage("water.png");
+		BufferedImage imgBuilding = con.loadImage("building.png");
+		BufferedImage imgHero = con.loadImage("hero.png");
+		BufferedImage imgEnemy = con.loadImage("enemy.png");
+		// Draw image
+		for(intRow = 0; intRow < 20; intRow++){
+			for(intColumn = 0; intColumn < 20; intColumn++){
+				if(strMap[intRow][intColumn].equals("g")){
+					con.drawImage(imgGrass, (intColumn * 20), (intRow * 20));
+				}else if(strMap[intRow][intColumn].equals("t")){
+					con.drawImage(imgTree, (intColumn * 20), (intRow * 20));
+				}else if(strMap[intRow][intColumn].equals("w")){
+					con.drawImage(imgWater, (intColumn * 20), (intRow * 20));
+				}else if(strMap[intRow][intColumn].equals("b")){
+					con.drawImage(imgBuilding, (intColumn * 20), (intRow * 20));
+				}else if(strMap[intRow][intColumn].equals("e")){
+					con.drawImage(imgEnemy, (intColumn * 20), (intRow * 20));
+				}
+			}
+		}
+
 	}
 	
 }
--- rpgmethods.java
+++ rpgmethods.java
@@ -2,7 +2,7 @@ import arc.*;
 
 public class rpgmethods{
 	
-	// Load world into 2D array
+	// Method: load world into 2D array
 	
 	public static String[][] loadWorld(String strFileName){
 		// Variables
@@ -10,16 +10,20 @@ public class rpgmethods{
 		int intCount2; // x-coordinate (increasing from left to right) 
 		TextInputFile txtMap = new TextInputFile(strFileName);
 		String strMap[][] = new String[20][20];
+		
+		
+		
 		// Load into array
-		for(intCount = 0; intCount < 20; intCount++){
+		/*for(intCount = 0; intCount < 20; intCount++){
 			for(intCount2 = 0; intCount2 < 20; intCount2++){
 				strMap[intCount2][intCount] = txtMap.readLine();
+				System.out.println("x "+intCount+"y "+intCount2+strMap[intCount2][intCount]);
 			}
-		}
+		} */
 		
 		return strMap;
 	}
 	
-	// 
+
 	
 }
