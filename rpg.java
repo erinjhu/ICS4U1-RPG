069cad69531514cff61623089ee4396a66e1f2bf
erin-hu24 <erinjhu@gmail.com>
2023-09-24 21:57:55 -0400

    Hero movement
    Char variable input in a while loop to change position of hero. Clears console and redraws map and hero after each movement. Issue: if the hero changes direction, it moves one square based off the previous keyboard input.

--- rpg.java
+++ rpg.java
@@ -14,10 +14,61 @@ public class rpg{
 		// Variables
 		Console con = new Console();
 		String[][] strMap = new String[20][20];
+		boolean blnAlive = true;
+		char chrPress = 'o'; // initialized to random number
 		// Load map into array
 		strMap = loadMap("map.csv");
 		// Display map on screen
 		displayMap(con, strMap);
+		// Drawing hero on initial position
+		BufferedImage imgHero = con.loadImage("hero.png");
+		con.drawImage(imgHero, (10 * 20), (3 * 20));
+		int intY = 60;
+		int intX = 200;
+		// Hero moves
+		while(blnAlive == true){
+			// Keyboard input
+			System.out.println("a: "+chrPress);
+			chrPress = con.getChar();
+			System.out.println("b: "+chrPress);
+			// Change position based on keyboard input
+			if(chrPress == 'w'){
+				intY = intY - 20;
+				con.repaint();
+				displayMap(con, strMap);
+				con.drawImage(imgHero, intX, intY);
+			}else if(chrPress == 'a'){
+				intX = intX - 20;
+				con.repaint();
+				displayMap(con, strMap);
+				con.drawImage(imgHero, intX, intY);
+			}else if(chrPress == 's'){
+				intY = intY + 20;
+				con.repaint();
+				displayMap(con, strMap);
+				con.drawImage(imgHero, intX, intY);
+			}else if(chrPress == 'd'){
+				intX = intX + 20;
+				con.repaint();
+				displayMap(con, strMap);
+				con.drawImage(imgHero, intX, intY);
+			}
+			System.out.println("X: "+intX+". Y: "+intY);
+			// Clear
+			System.out.println("c: "+chrPress);
+			chrPress = 'o';
+			System.out.println("d: "+chrPress);
+			con.clear();
+		}
+		
+		
+		// TO DO NEXT:
+			// 
+		
+		
+		//while(blnAlive == true){
+			//heroMove(con, intRow, intColumn);
+		//}
 	}
 	
 	// Method to load CSV into array
@@ -60,7 +111,6 @@ public class rpg{
 		BufferedImage imgTree = con.loadImage("tree.png");
 		BufferedImage imgWater = con.loadImage("water.png");
 		BufferedImage imgBuilding = con.loadImage("building.png");
-		BufferedImage imgHero = con.loadImage("hero.png");
 		BufferedImage imgEnemy = con.loadImage("enemy.png");
 		// Draw image
 		for(intRow = 0; intRow < 20; intRow++){
@@ -78,7 +128,31 @@ public class rpg{
 				}
 			}
 		}
-
 	}
 	
+	// Method to draw hero while using w,a,s,d keys to move
+	
+	/*public static void heroMove(Console con, int intRow, int intColumn){
+		// Variables
+		BufferedImage imgHero = con.loadImage("hero.png");
+		char chrPress;
+		// Draw image
+		con.drawImage(imgHero, (intColumn * 20), (intRow * 20));
+		// Char input to move hero
+		chrPress = con.getChar();
+		System.out.println(chrPress);
+		if(chrPress == 'w'){
+			intRow = intRow - 20;
+			con.drawImage(imgHero, (intColumn * 20), (intRow * 20));
+		}else if(chrPress == 'a'){
+			intColumn = intColumn - 20;
+			con.drawImage(imgHero, (intColumn * 20), (intRow * 20));
+		}else if(chrPress == 's'){
+			intRow = intRow + 20;
+			con.drawImage(imgHero, (intColumn * 20), (intRow * 20));
+		}else if(chrPress == 'd'){
+			intColumn = intColumn + 20;
+			con.drawImage(imgHero, (intColumn * 20), (intRow * 20));
+		}
+	} */
 }
